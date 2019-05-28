7fa50c6ec0c1daf499d20cee88fee434c82324fb
Aaron <aclee7221@gmail.com>
2019-05-28 09:26:04 -0700

    a

--- board.java
+++ board.java
@@ -3,9 +3,9 @@ import javax.swing.*;
 import java.util.*;
 public class board extends JFrame
 {
-    int border = 14;
+    int border = 11;
     JPanel p = new JPanel();
-    button[][] grid = new button[border][border];
+    button[][] grid = new button[border*2][border];
     String columnLabel = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     int counter = 0, rowLabel = 1;
     
@@ -16,16 +16,18 @@ public class board extends JFrame
     
     public board()
     {
-        setName( "CopyOfBattleship" );
-        setSize( 900, 900 );
+        setName( "board" );
+        setSize( 600, 1200 );
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout( new GridLayout( border + 1, border + 1, 1, 1) );
+        setLayout( new GridLayout( border*2 + 1, border + 1, 1, 1) );
         for( int i = 0; i < grid.length; i++ )
         {
             for( int j = 0; j < grid[i].length; j++ )
             {
-                if( i == 0 )
+                if( i == 0 || i == border)
                 {    
+                     if( counter == border )
+                        counter = 0;
                      String temp = columnLabel.substring( counter, counter + 1 );
                      counter++;
                      JLabel label = new JLabel( temp, SwingConstants.CENTER );
@@ -33,6 +35,8 @@ public class board extends JFrame
                 }               
                 else if ( j == 0 )
                 {
+                     if( rowLabel == border )
+                        rowLabel = 1;
                      JLabel label = new JLabel( String.valueOf(rowLabel), SwingConstants.CENTER );
                      rowLabel++;
                      add(label);
@@ -47,5 +51,5 @@ public class board extends JFrame
         add(p);
         setVisible(true);
     }
-
+    
 }
