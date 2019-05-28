58e6ffaa4adea2f9e00cd01be5e0f1c7a07370d1
Shaun Lane <nerdburglarstudios@gmail.com>
2019-05-28 10:02:15 -0700

    e

--- shipBoard.java
+++ shipBoard.java
@@ -2,7 +2,7 @@
 public class shipBoard{
 
     shipTile[][] game;
-    
+    int[] len = {5,4,3,3,2};
     public shipBoard(int x, int y){
         
         game = new shipTile[x][y];
@@ -26,20 +26,41 @@ public class shipBoard{
         return game;
         
     }
-    public boolean isValid(int r, int c){
-        
-        return r < game.length && r > -1 && c < game[0].length && c > -1 && game[r][c] == null;
+    public boolean isValid(int r, int c, int l){
+        boolean check = false;
+        for(int i: len){
+            if(i == l) check = true;
+            
+        }
+        return r < game.length && r > -1 && c < game[0].length && c > -1 && game[r][c] == null && check ;
         
         
     }
     
-    
-    public void setShip(int r, int c, int len, int rotate){
+    public int setShip(boolean i,int r1, int c1, int r2, int c2){
         
-         ship boat = new ship(len);
+        int diffH = Math.abs(r1 - r2);
+        
+        int diffLen = Math.abs(c1-2);
+         
+        int max = Math.max(diffH, diffLen);
+        int counter = 0;
+        for(int r = Math.min(r1,r2); r < game.length; r++){
+            for(int c = Math.min(c1,c2); c < game[0].length; c++){
+                //implement isValid
+                
+            }
+            
+            
+        }
+        return -1;
+    }
+    public void setShip(int r, int c, int le, int rotate){
+        
+         ship boat = new ship(le);
          if(rotate == 0 || rotate == 1){
               
-                for(int i = 0; i < len; i++){
+                for(int i = 0; i < le; i++){
              
              
                     if(rotate == 0 ){
@@ -58,7 +79,7 @@ public class shipBoard{
             }
             if(rotate == 2 || rotate == 3){
               
-                for(int i = 0; i < len; i++){
+                for(int i = 0; i < le; i++){
              
              
                     if(rotate == 2 ){
